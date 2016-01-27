0b076844db5a50719f1e6dba1694ed9f2c103062
Sonia7 <soniaverma89@rocketmail.com>
2016-01-27 15:15:49 -0500

    first commit

---
+++ Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+
+    public float speed = 5;
+
+    //PRIVATE INSTANCE VARIABLES
+    private Transform _transform;
+    private Vector3 _currentPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Make a reference with the Transform Component
+        this._transform = gameObject.GetComponent<Transform>();
+
+        // Reset the Ocean Sprite to the Top
+        this.Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._currentPosition = this._transform.position;
+        this._currentPosition -= new Vector3( this.speed,0);
+        this._transform.position = this._currentPosition;
+
+        if (this._currentPosition.x <= -720)
+        {
+            this.Reset();
+        }
+    }
+
+    public void Reset()
+    {
+        this._transform.position = new Vector3(720f, 0);
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5f;
+    private float _playerInput;
+    private Transform _transform;
+    private Vector2 _currentPosition;
+    // Use this for initialization
+    void Start()
+    {
+        this._transform = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._currentPosition = this._transform.position;
+
+
+
+        this._playerInput = Input.GetAxis("Horizontal");
+        if (this._playerInput > 0)
+        {
+            this._currentPosition += new Vector2(this.speed, 0);
+
+        }
+        if (this._playerInput < 0)
+        {
+            this._currentPosition -= new Vector2(this.speed, 0);
+        }
+
+        this._playerInput = Input.GetAxis("Vertical");
+        if (this._playerInput > 0)
+        {
+            this._currentPosition += new Vector2(0, this.speed);
+
+        }
+        if (this._playerInput < 0)
+        {
+            this._currentPosition -= new Vector2(0, this.speed);
+        }
+
+
+
+        if (this._currentPosition.x < -260)
+        {
+
+            this._currentPosition.x = -260;
+        }
+        if (this._currentPosition.x > 240)
+        {
+
+            this._currentPosition.x = 240;
+        }
+
+        if (this._currentPosition.y< -150)
+        {
+
+            this._currentPosition.y = -150;
+        }
+        if (this._currentPosition.y> 200)
+        {
+
+            this._currentPosition.y = 200;
+        }
+        this._transform.position = this._currentPosition;
+    }
+}
