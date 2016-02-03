77cd722efae53d0da71412116d2d2d061090eddc
Sonia7 <soniaverma89@rocketmail.com>
2016-02-03 13:33:35 -0500

    Enemy Added

--- Assets/_Scripts/EnemyController.cs
+++ Assets/_Scripts/EnemyController.cs
@@ -2,15 +2,46 @@
 using System.Collections;
 
 public class EnemyController : MonoBehaviour {
+    // PUBLIC INSTANCE VARIABLES
+    public float minVerticalSpeed = -1f;
+    public float maxVerticalSpeed = 1f;
+    public float minHorizontalSpeed = 4f;
+    public float maxHorizontalSpeed = 8f;
 
-    public Vector2 speed = new Vector2(50, 50);
-    public Vector2 direction = new Vector2(-1, 0);
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 movement = new Vector3(speed.x * direction.x, speed.y * direction.y, 0);
+    //PRIVATE INSTANCE VARIABLES
+    private Transform _transform;
+    private Vector2 _currentPosition;
+    private float _verticalDrift;
+    private float _horizontalSpeed;
 
-        movement *= Time.deltaTime;
-        transform.Translate(movement);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // Make a reference with the Transform Component
+        this._transform = gameObject.GetComponent<Transform>();
+
+        // Reset the Cloud Sprite to the Top
+        this.Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._currentPosition = this._transform.position;
+        this._currentPosition -= new Vector2(this._horizontalSpeed, this._verticalDrift);
+        this._transform.position = this._currentPosition;
+
+        if (this._currentPosition.x <= -343)
+        {
+            this.Reset();
+        }
+    }
+
+    public void Reset()
+    {
+        this._horizontalSpeed = Random.Range(this.minHorizontalSpeed, this.maxHorizontalSpeed);
+        this._verticalDrift = Random.Range(this.minVerticalSpeed, this.maxVerticalSpeed);
+        float yPosition = Random.Range(-200f, 200f);
+        this._transform.position = new Vector2(246f, yPosition);
+    }
 }
